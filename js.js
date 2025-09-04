295096c1b7db0f3f9333699c6c0f82940446cf63
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-09-04 15:40:31 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-    let code = prompt("test:");
+    let code = prompt("inject:");
     if (code.startsWith("javascript:")) {
       code = code.substring(11);
     }
