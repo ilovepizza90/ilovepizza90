d14d3b299ae38f4b29fb55eaa85c03efefe858de
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-10-28 21:25:51 -0500

    Create wow.js

---
+++ wow.js
@@ -0,0 +1,251 @@
+/// execute_script.js
+let runbase1 = unset
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    if (runbase1 == 67) {
+        let selected_mode = prompt("1 for global, 2 for crypto, 3 for fishing... compiled again, please pay my bills")
+        if (selected_mode == 1) {
+            let global = prompt("1 for subtle auto answer, 2 for ...")
+            if (global == 1) {
+(() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769908138;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        const { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        const Question = stateNode.state.question || stateNode.props.client.question;
+        let ind = 0;
+        while (ind < Question.answers.length) {
+            let j = 0;
+            let found = false;
+            while (j < Question.correctAnswers.length) {
+                if (Question.answers[ind] == Question.correctAnswers[j]) {
+                    found = true;
+                    break;
+                }
+                j++;
+            }
+            ind++;
+            if (found) document.querySelector("[class*='answersHolder'] :nth-child(" + ind + ") > div").style.boxShadow = "unset";
+        }
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/global/subtleHighlightAnswers.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        let i = 0;
+        while (i < data.length) {
+            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
+        try {
+            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
+        } catch (e) {}
+        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => {
+        img.onerror = img.onabort = null;
+        cheat();
+        let iframe = document.querySelector("iframe");
+        iframe.contentWindow.alert("ran.")
+    }
+})();
+/// end of script
+            } else if (global == 2) {
+                /// second script here
+                alert("not finished.")
+            }
+        } else if (selected_mode == 2) {
+            let crypto_selected = prompt("1 for crypto set, 2 for steal all")
+            if (crypto_selected == 1) {
+(() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769904085;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        let i = document.createElement('iframe');
+        document.body.append(i);
+        window.prompt = i.contentWindow.prompt.bind(window);
+        i.remove();
+        let amount = parseInt(prompt("How much crypto would you like?")) || 0;
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        stateNode.setState({ crypto: amount, crypto2: amount });
+        stateNode.props.liveGameController.setVal({
+            path: `c/${stateNode.props.client.name}/cr`,
+            val: amount
+        });
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/crypto/setCrypto.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        let i = 0;
+        while (i < data.length) {
+            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
+        try {
+            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
+        } catch (e) {}
+        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => {
+        img.onerror = img.onabort = null;
+        cheat();
+        let iframe = document.querySelector("iframe");
+        iframe.contentWindow.alert("ran.")
+    }
+})();
+            } else if (crypto_selected == 2) {
+                (() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769904015;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.setState({ hack: "" });
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/crypto/removeHack.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        let i = 0;
+        while (i < data.length) {
+            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
+        try {
+            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
+        } catch (e) {}
+        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => {
+        img.onerror = img.onabort = null;
+        cheat();
+        let iframe = document.querySelector("iframe");
+        iframe.contentWindow.alert("ran.")
+    }
+})();
+            }
+        } else if (selected_mode == 3) {
+            let fishing_selected = prompt("1 for frenzy global, 2 for set lure level 1-6, 3 for weight set")
+            if (fishing_selected == 1) {
+(() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769906350;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        stateNode.props.liveGameController.setVal({
+            path: `c/${stateNode.props.client.name}`,
+            val: {
+                b: stateNode.props.client.blook,
+                w: stateNode.state.weight,
+                f: "Frenzy",
+                s: true
+            }
+        })
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/Blooket-Council/Blooket-Cheats/main/autoupdate/timestamps/fishing/frenzy.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        let i = 0;
+        while (i < data.length) {
+            let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
+        try {
+            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
+        } catch (e) {}
+        if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => {
+        img.onerror = img.onabort = null;
+        cheat();
+        let iframe = document.querySelector("iframe");
+        iframe.contentWindow.alert("ran.")
+    }
+})();
+            }
+        }
+    } else {
+        let runbase1 = prompt()
+    }
+  }
+});
