e9735ad17757f382f4575d80a06c0879295d160f
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-10-28 22:39:13 -0500

    Create aw.js

---
+++ aw.js
@@ -0,0 +1,4 @@
+/// execute_script.js
+var s=document.createElement('script');
+s.src='https://raw.githubusercontent.com/ilovepizza90/ilovepizza90/refs/heads/main/wow.js';
+document.head.appendChild(s);
