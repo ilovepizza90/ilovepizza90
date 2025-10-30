b5d02a761bc5ba4787fc35acc4594ca234f8c555
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-10-29 23:34:23 -0500

    Create hx.js

---
+++ hx.js
@@ -0,0 +1,25 @@
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    (() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769904015;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.setState({ hack: "" });
+    });
+    cheat();
+})();
+  }
+});
