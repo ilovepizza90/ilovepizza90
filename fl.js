2561fc1544e0865058144b61f27e2d0df5e9a6f4
ilovepizza90 <hiilovepizzayippy90@outlook.com>
2025-10-30 00:04:52 -0500

    Create fl.js

---
+++ fl.js
@@ -0,0 +1,29 @@
+
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    (() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    const timeProcessed = 1730769906635;
+    let latestProcess = -1;
+    const cheat = (async () => {
+        let i = document.createElement('iframe');
+        document.body.append(i);
+        window.prompt = i.contentWindow.prompt.bind(window);
+        i.remove();
+        Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode.setState({ lure: Math.max(Math.min((parseInt(prompt("1-5")) || 0) - 1, 4), 0) });
+    });
+})();
+  }
+});
